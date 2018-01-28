744c4748b4c97c9c545e3e88491f7559a71d10fa
Andrewie <andreassundstr03m@gmail.com>
2018-01-28 15:03:25 +0100

    Initial commit

---
+++ Building_Escape_Game/Source/Building_Escape_Game/Building_Escape_GameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Building_Escape_GameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDING_ESCAPE_GAME_API ABuilding_Escape_GameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
